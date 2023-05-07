b7dbc4c0235a4de77c38a40f78da86b687abed33
Alisher Usmonov <83527629+Alisher-Usmonov@users.noreply.github.com>
2023-05-07 21:58:48 +0500

    feat: router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useStore } from "@/stores/store";
+
+const store = useStore();
+</script>
+
+<template>
+  <div class="text-2xl flex flex-col gap-y-2">
+    <h1>Home page {{ store.stars }}</h1>
+    <button
+      class="p-2 px-4 rounded-xl bg-gray-900 shadow-md border-3px border-gray-900 active:border-black"
+      @click="store.addStar()"
+    >
+      click
+    </button>
+  </div>
+</template>
